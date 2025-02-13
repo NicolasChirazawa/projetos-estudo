3941b0734129f24c0b886ccd5039abf27b6f8c85
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2025-02-13 00:22:43 -0300

    ♻️refactor: Reajuste nomes, responsabilidades e erros
    Foi reajustado o nome de quase todas as variáveis a fim de facilitar a compreensão mais clara do código.
    
    Foi dividido as funções em menores para concentrar suas responsabilidades dentro do seu escopo, tornando mais fácil a manutenção, como também a compreensão.
    
    Foi refeito o try-catch para um jeito que faz sentido.

--- precisao_acoes/codigo/funcoes_opcionais.js
+++ precisao_acoes/codigo/funcoes_opcionais.js
@@ -2,94 +2,112 @@ const { Erro } =  require('./classes.js')
 
 function ajustarDataCotacaoAPI(data){
     // É necessário ajustar o formato da data
-    let data_ajustada = {
+    let data_api = {
         data_inicial: data.data_inicial.split('-'),
         data_final: data.data_final.split('-')
     }
 
     // Transição: yyyy/mm/dd -> mm/dd/yyyy
-    data_ajustada.data_inicial = data_ajustada.data_inicial[1] + '/' + data_ajustada.data_inicial[2] + '/' + data_ajustada.data_inicial[0];
-    data_ajustada.data_final = data_ajustada.data_final[1] + '/' + data_ajustada.data_final[2] + '/' + data_ajustada.data_final[0];
+    data_api.data_inicial = data_api.data_inicial[1] + '/' + data_api.data_inicial[2] + '/' + data_api.data_inicial[0];
+    data_api.data_final = data_api.data_final[1] + '/' + data_api.data_final[2] + '/' + data_api.data_final[0];
 
-    return data_ajustada;
+    return data_api;
 }
 
-function verificacaoCotacaoResultado(lista_cotacao) {
-    let erro_codigo = 5;
-
-    if (lista_cotacao.value.length == 0){        
-        let erro = new Erro(erro_codigo);
-        return erro
-    } else {
-        return '';
-    }
-}
-
-function ajustarDataConversaoDolar(dicionario_cotacao, codigo_dicionario_cotacao, lista_cotacao){ 
-    // Ajustar o formato da data
-    for(let i = 0; i < lista_cotacao.value.length; i++){
-            
-        let data_tratada = String(lista_cotacao.value[i].dataHoraCotacao).split(' ')[0].split('-');
-        data_tratada = data_tratada[2] + '-' +  data_tratada[1] + '-' + data_tratada[0];
-
-        item_cotacao = {
-            dolar: String(lista_cotacao.value[i].cotacaoCompra),
-            data: data_tratada
-        }
-        dicionario_cotacao[item_cotacao.data] = item_cotacao;
-        codigo_dicionario_cotacao.push(item_cotacao.data);
-    }
-}
-
-function encontrarDataDolar(dicionario_cotacao, codigo_dicionario_cotacao){
-    let data_cotacao = '';
-
-    for(let i = 0; i < codigo_dicionario_cotacao.length; i++){
-        if(dicionario_cotacao[dados_tratados[i].data] != undefined){
-            data_cotacao = dicionario_cotacao[dados_tratados[i].data];
-            return data_cotacao;
-        }
-    }
-    return '';
-}
-
-async function cotacaoDolar(dados_tratados, data) {
-    let erro = '';
+function verificacaoCotacaoResultado(dados_brutos_cotacao) {
+    let erro_codigo = '012';
+    let erro;
 
     try{
-        let data_ajustada = ajustarDataCotacaoAPI(data);
-
-        // Pesquisa API
-        let pesquisa_cotacao = await fetch(`https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='${data_ajustada.data_inicial}'&@dataFinalCotacao='${data_ajustada.data_final}'&$top=100&$format=json&$select=cotacaoCompra,dataHoraCotacao`);
-    
-        let lista_cotacao = await pesquisa_cotacao.json();
-    
-        // Situação que não há nenhum resultado provido pela API dentro do espaço dado;
-        erro = verificacaoCotacaoResultado(lista_cotacao);
-        if(typeof erro === 'object'){
+        if (dados_brutos_cotacao.value.length == 0){        
+            erro = new Erro(erro_codigo);
             throw new Error(erro);
         }
-    
-        let dicionario_cotacao = {};
-        let codigo_dicionario_cotacao = [];
+        return false;
 
-        ajustarDataConversaoDolar(dicionario_cotacao, codigo_dicionario_cotacao, lista_cotacao);
-
-        let data_cotacao = encontrarDataDolar(dicionario_cotacao, codigo_dicionario_cotacao);
-
-        // Caso não encontre nenhun igual
-        if(data_cotacao == ''){
-            data_cotacao = lista_cotacao.value[0].cotacaoCompra;
-        }
-
-        for(let i = 0; i < dados_tratados.length; i++){
-            dados_tratados[i].alta = (Number(dados_tratados[i].alta) * Number(data_cotacao.dolar));
-            dados_tratados[i].baixa = (Number(dados_tratados[i].baixa) * Number(data_cotacao.dolar));
-            dados_tratados[i].abertura = (Number(dados_tratados[i].abertura) * Number(data_cotacao.dolar));
-            dados_tratados[i].fechamento = (Number(dados_tratados[i].fechamento) * Number(data_cotacao.dolar));
-        }
     } catch {
         console.log(erro.lancarMensagem());
+        return true;
     }
 }
-module.exports = { cotacaoDolar }
+
+function tratarDadosConversaoDolar (dados_brutos_cotacao, dados_tratados_cotacao, codigo_data_cotacao){ 
+
+    for(let i = 0; i < dados_brutos_cotacao.value.length; i++){   
+
+        // yyyy/mm/dd horário -> dd/mm/yyyy
+        let data_tratada = String(dados_brutos_cotacao.value[i].dataHoraCotacao).split(' ')[0].split('-');
+        data_tratada = data_tratada[2] + '-' +  data_tratada[1] + '-' + data_tratada[0];
+
+        cotacao_unidade = {
+            dolar: String(dados_brutos_cotacao.value[i].cotacaoCompra),
+            data: data_tratada
+        }
+
+        dados_tratados_cotacao[cotacao_unidade.data] = cotacao_unidade;
+        codigo_data_cotacao.push(cotacao_unidade.data);
+    }
+}
+
+function encontrarDataDolar(dados_tratados_cotacao, codigo_data_cotacao, dados_acoes){
+    let data_cotacao = '';
+
+    for(let i = 0; i < codigo_data_cotacao.length; i++){
+
+        if(dados_tratados_cotacao[dados_acoes[i].data] != undefined){
+            data_cotacao = dados_tratados_cotacao[dados_acoes[i].data];
+            break;
+        }
+    }
+    return data_cotacao;
+}
+
+function converterAcaoDolar(dados_acoes, data_cotacao){
+    for(let i = 0; i < dados_acoes.length; i++) {
+
+        // Obtem o resultado convertido
+        dados_acoes[i].alta = (Number(dados_acoes[i].alta) * Number(data_cotacao.dolar));
+        dados_acoes[i].baixa = (Number(dados_acoes[i].baixa) * Number(data_cotacao.dolar));
+        dados_acoes[i].abertura = (Number(dados_acoes[i].abertura) * Number(data_cotacao.dolar));
+        dados_acoes[i].fechamento = (Number(dados_acoes[i].fechamento) * Number(data_cotacao.dolar));
+
+        // Tratar cadas decimais
+        let dado_alta_nao_tratado = String(dados_acoes[i].alta).split('.');
+        let dado_baixa_nao_tratado = String(dados_acoes[i].baixa).split('.');
+        let dado_abertura_nao_tratado = String(dados_acoes[i].abertura).split('.');
+        let dado_fechamento_nao_tratado = String(dados_acoes[i].fechamento).split('.');
+
+        dados_acoes[i].alta = dado_alta_nao_tratado[0] + '.' + dado_alta_nao_tratado[1].slice(0, 2);
+        dados_acoes[i].baixa = dado_baixa_nao_tratado[0] + '.' + dado_baixa_nao_tratado[1].slice(0, 2);
+        dados_acoes[i].abertura = dado_abertura_nao_tratado[0] + '.' + dado_abertura_nao_tratado[1].slice(0, 2);
+        dados_acoes[i].fechamento = dado_fechamento_nao_tratado[0] + '.' + dado_fechamento_nao_tratado[1].slice(0, 2);
+    }
+}
+
+async function converterDolar(dados_acoes, data) {
+
+    let data_api = ajustarDataCotacaoAPI(data);
+
+    // Pesquisa API
+    let pesquisa_cotacao_api = await fetch(`https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='${data_api.data_inicial}'&@dataFinalCotacao='${data_api.data_final}'&$top=100&$format=json&$select=cotacaoCompra,dataHoraCotacao`);
+
+    let dados_brutos_cotacao = await pesquisa_cotacao_api.json();
+
+    let naoEncontrouCotacao = verificacaoCotacaoResultado(dados_brutos_cotacao);
+    if(naoEncontrouCotacao == true) { return false }
+
+    let dados_tratados_cotacao = {};
+    let codigo_data_cotacao = [];
+
+    tratarDadosConversaoDolar(dados_brutos_cotacao, dados_tratados_cotacao, codigo_data_cotacao);
+
+    let data_cotacao = encontrarDataDolar(dados_tratados_cotacao, codigo_data_cotacao, dados_acoes);
+
+    // Pega o primeiro resultado disponível (sempre haverá, foi feito o teste 'verificacaoCotacaoResultado()' para confirmar).
+    if(data_cotacao == '') { data_cotacao = dados_brutos_cotacao.value[0].cotacaoCompra }
+
+    converterAcaoDolar(dados_acoes, data_cotacao);
+
+    return true;
+}
+module.exports = { converterDolar }
