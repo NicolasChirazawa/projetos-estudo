be0d973be2e12fedd7bc66ba7003b5d68b83b9e0
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2025-02-13 00:19:09 -0300

    ✨ feat: Adição de um novo erro

--- precisao_acoes/codigo/classes.js
+++ precisao_acoes/codigo/classes.js
@@ -19,7 +19,8 @@ class Erro {
             '003': 'A data inicial é maior a data final.',
             '004': 'Não foi achado nenhuma ação dentro do código e período inserido',
             // 01x: Dados impossíveis de serem retornados
-            '011': 'Não foi possível converter a moeda para real'
+            '011': 'Não foi encontrado ações dentro do período indicado',
+            '012': 'Não foi possível converter a moeda para real'
         }
     }
 
